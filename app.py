1e94cdb1bb3ad8762862f396a0167e69077e7ac7
Minyeob Kim <kimmy9809@gmail.com>
2022-06-03 14:22:09 +0900

    add keras model

--- app.py
+++ app.py
@@ -1,14 +1,37 @@
+from typing import List
+import numpy as np
 import pandas as pd
+from datetime import date
 import streamlit as st
 import streamlit.components.v1 as components
 import plotly.figure_factory as ff
 import plotly.graph_objects as go
 import plotly.express as px
-from datetime import datetime, date
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.models import load_model as load_keras_model
+
+CLASS = {0:'정상', 1:'경증', 2:'치매'}
+
+
+def make_input_data(df: pd.DataFrame, email: str) -> np.ndarray:
+    to_pad = 93
+    sequences = []
+
+    for one_seq in [np.array(df[df.EMAIL == email].drop(['EMAIL','요약 날짜'],axis=1))]:
+        n = to_pad - len(sequences)
+        to_concat = np.repeat(one_seq[-1], n).reshape(51, n).transpose()
+        new_one_seq = np.concatenate([one_seq, to_concat])
+        sequences.append(new_one_seq)
+    final_seq = np.stack(sequences)
+
+    seq_len = 72
+    final_seq = sequence.pad_sequences(final_seq, maxlen=seq_len, padding='post', dtype='float', truncating='post')
+    return final_seq
 
 
 def main():
 
+    model = load_keras_model('best_modelacc.h5')
     st.set_page_config(layout="wide")
 
     ##############################################################
@@ -27,17 +50,18 @@ def main():
     with email:
         if file:
             email = st.selectbox('이메일 주소', df.EMAIL.drop_duplicates().tolist(), key='email')
+            select_df = df[df['EMAIL'] == email].copy()
         else:
             st.write('이메일 주소')
     with sdate:
-        if file:
-            sdate = st.selectbox('일자', ['전체']+df['요약 날짜'].tolist(), key='sdate')
-        else:
-            st.write('일자')
-        # st.write(date.today().strftime('%Y-%m-%d'))
+        date_list = ['전체']+df['요약 날짜'].tolist() if file else [date.today().strftime('%Y-%m-%d')]
+        sdate = st.selectbox('일자', date_list, key='sdate')
     with pred:
         if file:
-            st.write('분석결과 치매일 확률은 23.54% 입니다.')
+            input_data = make_input_data(df, email)
+            pred = model.predict(input_data)
+            output = (CLASS[np.argmax(pred)], round(max(pred[0]),2))
+            st.write(f'분석결과 {output[1]}% 확률로 {output[0]} 입니다.')
         else:
             st.write('파일을 업로드 해주세요.')
 
@@ -49,10 +73,10 @@ def main():
     if file:
         st.markdown('---')
         if sdate != '전체':
-            df = df[(df['EMAIL'] == email) & (df['요약 날짜'] == sdate)]
+            select_df = df[(df['EMAIL'] == email) & (df['요약 날짜'] == sdate)]
         else:
-            df = df[(df['EMAIL'] == email)]
-        st.dataframe(df)
+            select_df = df[(df['EMAIL'] == email)]
+        st.dataframe(select_df)
         st.markdown('---')
 
         ##############################################################
@@ -62,18 +86,18 @@ def main():
         left, center, right = st.columns([1,1,1])
         with left:
             labels = ['깊은 수면 시간','램수면 시간','잠 시간']
-            values = [df[label].mean().round(2) for label in labels]
+            values = [select_df[label].mean().round(2) for label in labels]
             values[2] = values[2] - values[0] - values[1]
             sleep_time_chart = go.Figure(go.Pie(labels=labels,values=values))
             st.plotly_chart(sleep_time_chart,use_container_width=True)
         with center:
             labels = ['매일 움직인 거리', '매일 걸음 수']
-            values = [df[label].mean().round(2) for label in labels]
+            values = [select_df[label].mean().round(2) for label in labels]
             dist_step_chart = go.Figure(go.Bar(x=values,y=labels,orientation='h'))
             st.plotly_chart(dist_step_chart,use_container_width=True)
         with right:
             labels = ['고강도 활동 시간', '중강도 활동 시간', '저강도 활동 시간']
-            values = [df[label].mean().round(2) for label in labels]
+            values = [select_df[label].mean().round(2) for label in labels]
             act_time_chart = go.Figure(go.Bar(x=values,y=labels,orientation='h'))
             st.plotly_chart(act_time_chart,use_container_width=True)
 
@@ -86,13 +110,13 @@ def main():
         radar_left, radar_right = st.columns([1,1])
         with radar_left:
             labels = ['활동 점수', '활동 목표달성 점수', '활동 유지 점수', '운동 빈도 점수', '운동 볼륨 점수']
-            values = [df[label].mean().round(2) for label in labels]
+            values = [select_df[label].mean().round(2) for label in labels]
             active_df = pd.DataFrame(dict(theta=labels,r=values))
             active_score_chart = px.line_polar(active_df, r='r', theta='theta', line_close=True, range_r=[0,100])
             st.write(active_score_chart)
         with radar_right:
             labels = ['램수면 점수', '깊은 수면 점수', '수면 시기 점수', '수면 방해 점수', '수면 효율 점수', '수면 잠복 점수'   ]
-            values = [df[label].mean().round(2) for label in labels]
+            values = [select_df[label].mean().round(2) for label in labels]
             sleep_df = pd.DataFrame(dict(theta=labels,r=values))
             sleep_score_chart = px.line_polar(sleep_df, r='r', theta='theta', line_close=True, range_r=[0,100])
             st.write(sleep_score_chart)
